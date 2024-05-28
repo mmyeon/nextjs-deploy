b86b51314eda7b855373f50e62ecb789b43aed58
mmyeon <mindfulyeon@gmail.com>
2024-05-28 18:59:28 +0900

    [chore] Change content

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>This is time for deployment.</h1>;
+  return;
+  <>
+    <h1>This is time for deployment.</h1>
+    <p>Finally I did it!!!</p>
+  </>;
 }
